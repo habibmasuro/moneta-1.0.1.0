483b93deb5e902cd23b7cfb8b919d13b1ad5300d
habibmasuro <habib.masuro@gmail.com>
2015-09-15 23:08:26 +0300

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both monetad and moneta-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Tellurion");
+const std::string CLIENT_NAME("MONETA.io");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
